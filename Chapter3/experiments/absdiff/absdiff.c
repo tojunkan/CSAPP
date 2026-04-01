6d5870c3c22bc8e1954d949bb06579a0802d856e
tojunkan <durunhan12345@126.com>
2026-04-01 14:35:16 +0800

    added some annotations.

--- Chapter3/experiments/absdiff/absdiff.c
+++ Chapter3/experiments/absdiff/absdiff.c
@@ -31,3 +31,63 @@ x_ge_y:
 	result = x - y;
 	return result;
 }
+
+/*
+ * run objdump -d and you can find that:
+ * (note: the assembly code for function absdiff and gotodiff is totally same.)
+ * This indicates that GCC will deal conditional sentences like:
+ * test/cmp condition:
+ * 	jmp(if not condition) to FALSE:
+ * 	then sentences;
+ * 	jmp to DONE;
+ * FALSE:
+ * 	else sentences;
+ * DONE:
+ * 	...
+ * 0000000000000000 <absdiff>:
+   0:   f3 0f 1e fa             endbr64
+   4:   39 f7                   cmp    %esi,%edi
+   6:   7d 0d                   jge    15 <absdiff+0x15>
+   8:   48 83 05 00 00 00 00    addq   $0x1,0x0(%rip)        # 10 <absdiff+0x10>
+   				^
+			       /_\
+			        |
+				|
+				the source code is ++(increament), but we can see that GCC assembled it to
+				the instruction "addq". that's because instruction "inc" does not update 
+				some of thosecondition flags such as OF, ZF, but will not update the remain.
+			        (this is due to some historical issue)	
+				This may cause mismatching of those conditiona flags, so now CPU will take time
+				to deal with it, and the program will turn out to be slower.
+   f:   01
+  10:   89 f0                   mov    %esi,%eax
+  12:   29 f8                   sub    %edi,%eax
+  14:   c3                      ret
+  15:   48 83 05 00 00 00 00    addq   $0x1,0x0(%rip)        # 1d <absdiff+0x1d>
+  					    ^
+					   /_\
+					    |
+					    |
+					    here we can see 0x0(%rip), that's because we defined variables in 
+					    the global field. These variables will have a static position and 
+					    will not be free till the program is over.
+					    so, we can use PC-relative way to calculate it's index.
+					    the register %rip always points to the next instructions to be processed.
+					    (where r represents it's 64bits, 
+					    and ip is the abbreviation for "instruction pointer")
+					    note that %rip is different from the general registers, that it is
+					    unwrittenable. you can't write
+					    movq %rax, %rip to change its value.
+					    but you can use conditional jumps, like:
+					    jmp *%rax, then the program will jump to the address where %rax points to, 
+					    indirectly changing register %rip.
+
+
+					    the number 0x0 is the offset from %rip.
+					    here assembler remains the number with 0x0, that's because the object file
+					    is still not linked, so the actual address is still unkown.
+  1c:   01
+  1d:   89 f8                   mov    %edi,%eax
+  1f:   29 f0                   sub    %esi,%eax
+  21:   c3                      ret
+  */
